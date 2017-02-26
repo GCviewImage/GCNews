5457210f9eeb1c0bf220675f3545cfcfbe9ed450
IOS <1065820795@qq.com>
2017-02-26 14:39:45 +0800

    初始化项目

---
+++ GCNews/GCNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCNews
+//
+//  Created by p on 17/2/26.
+//  Copyright © 2017年 gongcheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCNews/GCNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCNews
+//
+//  Created by p on 17/2/26.
+//  Copyright © 2017年 gongcheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
