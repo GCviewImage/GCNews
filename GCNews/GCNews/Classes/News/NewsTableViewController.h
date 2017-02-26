fa6207b9b25a38926ddc3d942682e5ea41462697
IOS <1065820795@qq.com>
2017-02-26 15:07:09 +0800

    搭建项目框架

--- GCNews/GCNews/Classes/News/NewsTableViewController.h
+++ GCNews/GCNews/Classes/News/NewsTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  NewsTableViewController.h
 //  GCNews
 //
 //  Created by p on 17/2/26.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface NewsTableViewController : UITableViewController
 
 @end
-
